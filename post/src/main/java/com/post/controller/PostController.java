226544d5c08ba02c15c635b57f81c6f540f3ba59
farzanraza <farzanraza99@gmail.com>
2023-10-08 13:57:16 +0530

    eureka-gateway-circuir

--- post/src/main/java/com/post/controller/PostController.java
+++ post/src/main/java/com/post/controller/PostController.java
@@ -3,6 +3,7 @@ package com.post.controller;
 import com.post.entity.Post;
 import com.post.payload.PostDto;
 import com.post.service.PostService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,9 +27,21 @@ public class PostController {
         return new ResponseEntity<>(record,HttpStatus.OK);
     }
     @GetMapping("/{postId}/comments")
+    @CircuitBreaker(name = "commentBreaker",fallbackMethod = "commentFallback")
     public  ResponseEntity<PostDto> getAllCommentForPost(@PathVariable String postId){
         PostDto allCommentForPost = postService.getAllCommentForPost(postId);
         return new ResponseEntity<>(allCommentForPost,HttpStatus.OK);
+    }
+    // same handler method as for getAllCommentForPost
+    public ResponseEntity<PostDto> commentFallback(String postId,Exception ex){
+        System.out.println("Fallback is executed because server is down:"+ex.getMessage());
+        ex.printStackTrace();;
+        PostDto postDto=new PostDto();
+        postDto.setId("12345");
+        postDto.setTitle("service down");
+        postDto.setContent("service down");
+        postDto.setDescription("service down");
+        return new ResponseEntity<>(postDto,HttpStatus.BAD_REQUEST);
     }
 
 }
